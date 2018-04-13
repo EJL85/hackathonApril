284ac583fb2a441db48fdad9f96d3d8f96239dca
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-04-13 14:30:43 -0600

    added whiskey

---
+++ scratch.js
@@ -0,0 +1,19 @@
+const theSurprise = document.getElementById('random');
+
+theSurprise.addEventListener('click', displayWhiskey);
+
+function displayWhiskey(event) {
+  event.preventDefault();
+  const $img = document.createElement('img');
+  const $h1 = document.createElement('h1');
+  const $section = document.getElementById('recipe');
+
+  $img.src = '/pics/scotch.jpg';
+  $img.id = 'whiskey'
+  $h1.id = 'birthdayH1'
+  $section.appendChild($h1);
+  $section.appendChild($img);
+
+  $h1.textContent = "Happy Birthday Jeff!"
+  
+}
